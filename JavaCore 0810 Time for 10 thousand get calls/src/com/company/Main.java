4e20aec019caf5dd5c63c6b5263184b0b5085b40
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-17 10:37:23 +0200

    Add files via upload

---
+++ JavaCore 0810 Time for 10 thousand get calls/src/com/company/Main.java
@@ -0,0 +1,54 @@
+package com.company;
+
+/*
+
+0810. Время для 10 тысяч вызовов get
+
+Измерить, сколько времени занимает 10 тысяч вызовов get для каждого списка.
+Метод getTimeMsOfGet должен вернуть время своего исполнения в миллисекундах.
+Требования:
+1. Программа должна выводить числа на экран.
+2. Метод main должен вызывать метод getTimeMsOfGet только два раза.
+3. Метод main должен вызывать метод fill только два раза.
+4. Метод fill(List list) должен вставлять 10 тысяч элементов в список.
+5. Метод get10000(List list) должен производить 10 тысяч вызовов get для списка.
+6. Метод getTimeMsOfGet должен вызывать метод get10000(List list) только один раз.
+7. Метод getTimeMsOfGet должен вернуть время в миллисекундах, которое занимает 10 тысяч вызовов get для списка.
+
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println(getTimeMsOfGet(fill(new ArrayList())));
+        System.out.println(getTimeMsOfGet(fill(new LinkedList())));
+    }
+    public static List fill(List list) {
+        for (int i=0; i<10000; i++) {
+            list.add(new Object());
+        }
+        return list;
+    }
+    public static long getTimeMsOfGet(List list) {
+        Date currentDate = new Date();
+        get10000(list);
+        return new Date().getTime()-currentDate.getTime();
+    }
+    public static void get10000(List list) {
+        if(list.isEmpty()) return;
+        int x=list.size() / 2;
+        for (int i=0; i<10000; i++) {
+            list.get(x);
+        }
+    }
+
+}
+
+
+
+
